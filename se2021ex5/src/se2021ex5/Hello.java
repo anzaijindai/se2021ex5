55103d131e6668ac27127e15479b799da3e2cb65
anzai <r202003439yi@jindai.jp>
2022-01-18 15:27:20 +0900

    this is version3

--- se2021ex5/src/se2021ex5/Hello.java
+++ se2021ex5/src/se2021ex5/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		String name; name = "anzai";
-		System.out.println("Hello World" + name);
+		int age; age = 20;
+		System.out.println("Hello World"+name+" "+age);
 	}
 }
